17b5c483d16a27a97dd05bbd4a50454fb0c4d2d7
sagarapidev <sagar.bhuiya@optum.com>
2023-02-03 05:02:35 +0530

    How to Read external properties file in springboot?

---
+++ src/main/java/com/webapiserver/config/ConfigProperty.java
@@ -0,0 +1,28 @@
+package com.webapiserver.config;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:global.config.properties"),
+        @PropertySource("classpath:config.properties")
+})
+public class ConfigProperty {
+    @Value("${ENV_USER_HOME}")
+    String userHome;
+    @Value("${ENV_USER_DIR}")
+    String userDir;
+    @Value("${LOG_FILENAME}")
+    String logfile;
+
+    @Value("${GLOBAL_CONFIG_MESSAGE}")
+    String globalConfigMessage;
+}
---
+++ src/test/java/com/webapiserver/ConfigPropertyTest.java
@@ -0,0 +1,27 @@
+package com.webapiserver;
+
+import com.webapiserver.config.ConfigProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest
+@Slf4j
+public class ConfigPropertyTest {
+    @Autowired
+    ConfigProperty configProperty;
+
+    @Autowired
+    Environment environment;
+
+    @Test
+    void envUserHome() {
+        log.info("user.home:{}",configProperty.getLogfile());
+        log.info("user.dir:{}",configProperty.getUserDir());
+        log.info("user.logfile:{}",configProperty.getLogfile());
+        log.info("global.config:{}",configProperty.getGlobalConfigMessage());
+
+    }
+}
