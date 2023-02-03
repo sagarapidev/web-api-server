f2067c928d46a3c73dc7d356471fbfcc82f9af03
sagarapidev <sagar.bhuiya@optum.com>
2023-02-04 03:51:12 +0530

    user.dir back slash

--- src/main/java/com/webapiserver/service/CsvService.java
+++ src/main/java/com/webapiserver/service/CsvService.java
@@ -3,7 +3,9 @@ package com.webapiserver.service;
 import com.webapiserver.domain.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CsvService {
     List<User> fetchAllUser();
+    List<Map<String, Object>> getResultSet(String query);
 }
--- src/main/java/com/webapiserver/service/imp/UserService.java
+++ src/main/java/com/webapiserver/service/imp/UserService.java
@@ -4,16 +4,28 @@ import com.webapiserver.domain.User;
 import com.webapiserver.repository.UserRepository;
 import com.webapiserver.service.CsvService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService implements CsvService {
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     @Override
     public List<User> fetchAllUser() {
         return (List<User>) userRepository.findAll();
     }
+
+    @Override
+    public List<Map<String, Object>> getResultSet(String query) {
+        return jdbcTemplate.queryForList(query);
+    }
+
+
 }
